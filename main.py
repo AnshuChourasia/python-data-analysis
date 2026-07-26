f6d6bb1e456836f3039de12acd44097537d8933f
AnshuChourasia <anshuchourasia23804@gmail.com>
2026-07-26 19:03:15 +0530

    Add files via upload
    Pandas GroupBy and Aggregation

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
 import pandas as pd
 employees = {
-    "ID":[1,2,3,4,5,6],
-    "Name":["Anshu","Jay","Alina","Soni","Ava","Vivek"],
-    "Department":["IT","HR","IT","Finance","HR","IT"],
-    "Salary":[50000,60000,None,55000,58000,62000]
+    "Name": ["Anshu","Jay","Alina","Soni","Ava","Vivek","Radhika","Diya"],
+    "Department": ["IT","HR","IT","Finance","HR","IT","Finance","IT"],
+    "City": ["Delhi","Delhi","Mumbai","Mumbai","Delhi","Mumbai","Delhi","Delhi"],
+    "Salary": [50000,60000,55000,70000,58000,62000,68000,65000]
 }
+
 df=pd.DataFrame(employees)
 print(df)
-print(df.head())
-print(df.tail())
-print(df.info())
-print(df.describe())
-print(df["Name"])
-print(df[["Name","Salary"]])
-print(df[df["Salary"]>55000])
-print(df[df["Department"]=="IT"])
-print(df.sort_values(by="Salary",ascending=False))
-print(df[(df["Salary"]>52000) & (df["Salary"]<57000)])
-print(df[
-    ((df["Department"]=="IT") | (df["Department"]=="HR")) & (df["Salary"]>55000)
-    ])
-print(df["Salary"].mean())
-highest_salary=df.sort_values(by="Salary",ascending=False)
-print(highest_salary.head(3))
-print(df["Department"].value_counts())
 
-print(df.isnull())
-print(df.isnull().sum())
-df=df.fillna(df["Salary"].mean())
-df["Salary"]+=3000
-df["Bonus"]=df["Salary"] * 0.10
-df["Tax"]=df["Salary"] * 0.05
-print(df)
+print(df.groupby("Department")["Salary"].mean())
+print(df.groupby("Department")["Salary"].max())
+print(df.groupby("Department")["Salary"].sum())
+print(df.groupby("Department").count())
+print(df.groupby(["Department","City"])["Salary"].mean())
+print(df[["Department","City"]].value_counts())
+print(df.groupby("Department")["Salary"].agg(["mean","max","min"]))
 
-df.rename(columns={
-          "Department":"Team",
-          "Salary":"Monthly Salary"},inplace=True)
-df.drop(columns=["Tax"],inplace=True)
-
-df.to_csv("employee_report.csv",index=False)
+result=df.groupby("Department").value_counts().reset_index()
+print(result)
